25292d9a7ad5814f5a2df2a66d58214c72879120
Develop-Farhana <Coderfarhana99@gmail.com>
2024-07-06 19:25:11 +0530

    done

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,13 +1,133 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel AJAX CRUD with DataTable</title>
+@extends('includes.layout')
+
+@section('style')
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <style>
+        /* Custom CSS for DataTable */
+        .data-table {
+            width: 100%;
+        }
+
+        .data-table thead th {
+            text-align: center;
+        }
+
+        .data-table tbody td {
+            vertical-align: middle;
+        }
+
+        .data-table img.img-thumbnail {
+            max-width: 70px;
+            height: auto;
+        }
+
+        .btn {
+            cursor: pointer;
+        }
+
+        .modal-content {
+            border-radius: 0;
+        }
+
+        .modal-header {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        .modal-title {
+            color: #fff;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+
+        .btn-danger {
+            background-color: #dc3545;
+            border-color: #dc3545;
+        }
+
+        .btn-danger:hover {
+            background-color: #c82333;
+            border-color: #bd2130;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="container">
+        <h2>Products</h2>
+        <a class="btn btn-success" href="javascript:void(0)" id="createNewProduct"> Create New Product</a>
+        <table class="table table-bordered data-table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Image</th>
+                    <th width="280px">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="modal fade" id="ajaxModel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modelHeading"></h4>
+                </div>
+                <div class="modal-body">
+                    <form id="productForm" name="productForm" class="form-horizontal">
+                        <input type="hidden" name="product_id" id="product_id">
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">Name</label>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="" maxlength="50" required="">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="category_id" class="col-sm-2 control-label">Category</label>
+                            <div class="col-sm-12">
+                                <select class="form-control" id="category_id" name="category_id" required="">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="image" class="col-sm-2 control-label">Image</label>
+                            <div class="col-sm-12">
+                                <input type="file" class="form-control" id="image" name="image">
+                            </div>
+                        </div>
+
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
@@ -18,161 +138,96 @@
     <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-    <h2>Laravel AJAX CRUD with DataTable</h2>
-    <a class="btn btn-success" href="javascript:void(0)" id="createNewProduct"> Create New Product</a>
-    <table class="table table-bordered data-table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Image</th>
-                <th width="280px">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-</div>
 
-<div class="modal fade" id="ajaxModel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="modelHeading"></h4>
-            </div>
-            <div class="modal-body">
-                <form id="productForm" name="productForm" class="form-horizontal">
-                   <input type="hidden" name="product_id" id="product_id">
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">Name</label>
-                        <div class="col-sm-12">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="" maxlength="50" required="">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="category_id" class="col-sm-2 control-label">Category</label>
-                        <div class="col-sm-12">
-                            <select class="form-control" id="category_id" name="category_id" required="">
-                                @foreach($categories as $category)
-                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="image" class="col-sm-2 control-label">Image</label>
-                        <div class="col-sm-12">
-                            <input type="file" class="form-control" id="image" name="image">
-                        </div>
-                    </div>
-
-                    <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script type="text/javascript">
-$(function () {
-
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    var table = $('.data-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('products.getProducts') }}",
-        columns: [
-            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-            {data: 'name', name: 'name'},
-            {data: 'category.name', name: 'category.name'},
-            {data: 'image', name: 'image', 
-                render: function(data, type, full, meta){
-                    return "<img src={{ URL::to('/') }}/images/" + data + " width='70' class='img-thumbnail' />";
-                }
-            },
-            {data: 'action', name: 'action', orderable: false, searchable: false},
-        ]
-    });
-
-    $('#createNewProduct').click(function () {
-        $('#saveBtn').val("create-product");
-        $('#product_id').val('');
-        $('#productForm').trigger("reset");
-        $('#modelHeading').html("Create New Product");
-        $('#ajaxModel').modal('show');
-    });
-
-    $('body').on('click', '.editProduct', function () {
-      var product_id = $(this).data('id');
-      $.get("{{ route('products.index') }}" +'/' + product_id +'/edit', function (data) {
-          $('#modelHeading').html("Edit Product");
-          $('#saveBtn').val("edit-product");
-          $('#ajaxModel').modal('show');
-          $('#product_id').val(data.id);
-          $('#name').val(data.name);
-          $('#category_id').val(data.category_id);
-          // If you need to display the image, you can use the following line
-          // $('#image').val(data.image);
-      })
-    });
-
-    $('#saveBtn').click(function (e) {
-        e.preventDefault();
-        $(this).html('Sending..');
-
-        var formData = new FormData($('#productForm')[0]);
-
-        $.ajax({
-          data: formData,
-          url: "{{ route('products.store') }}",
-          type: "POST",
-          contentType: false,
-          processData: false,
-          success: function (data) {
-              $('#productForm').trigger("reset");
-              $('#ajaxModel').modal('hide');
-              table.draw();
-          },
-          error: function (data) {
-              console.log('Error:', data);
-              $('#saveBtn').html('Save Changes');
-          }
-      });
-    });
-
-    $('body').on('click', '.deleteProduct', function () {
-        var product_id = $(this).data("id");
-        var result = confirm("Are you sure want to delete this product?");
-        if(result) {
-            $.ajax({
-                type: "DELETE",
-                url: "{{ route('products.store') }}"+'/'+product_id,
-                success: function (data) {
-                    table.draw();
-                },
-                error: function (data) {
-                    console.log('Error:', data);
+    <script type="text/javascript">
+        $(function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
-        }
-    });
 
-});
-</script>
+            var table = $('.data-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('products.getProducts') }}",
+                columns: [
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                    {data: 'name', name: 'name'},
+                    {data: 'category.name', name: 'category.name'},
+                    {
+                        data: 'image', name: 'image',
+                        render: function(data, type, full, meta){
+                            return "<img src={{ URL::to('/') }}/images/" + data + " width='70' class='img-thumbnail' />";
+                        }
+                    },
+                    {data: 'action', name: 'action', orderable: false, searchable: false},
+                ]
+            });
 
-</body>
-</html>
+            $('#createNewProduct').click(function () {
+                $('#saveBtn').val("create-product");
+                $('#product_id').val('');
+                $('#productForm').trigger("reset");
+                $('#modelHeading').html("Create New Product");
+                $('#ajaxModel').modal('show');
+            });
+
+            $('body').on('click', '.editProduct', function () {
+                var product_id = $(this).data('id');
+                $.get("{{ route('products.index') }}" +'/' + product_id +'/edit', function (data) {
+                    $('#modelHeading').html("Edit Product");
+                    $('#saveBtn').val("edit-product");
+                    $('#ajaxModel').modal('show');
+                    $('#product_id').val(data.id);
+                    $('#name').val(data.name);
+                    $('#category_id').val(data.category_id);
+                    // If you need to display the image, you can use the following line
+                    // $('#image').val(data.image);
+                })
+            });
+
+            $('#saveBtn').click(function (e) {
+                e.preventDefault();
+                $(this).html('Sending..');
+
+                var formData = new FormData($('#productForm')[0]);
+
+                $.ajax({
+                    data: formData,
+                    url: "{{ route('products.store') }}",
+                    type: "POST",
+                    contentType: false,
+                    processData: false,
+                    success: function (data) {
+                        $('#productForm').trigger("reset");
+                        $('#ajaxModel').modal('hide');
+                        table.draw();
+                    },
+                    error: function (data) {
+                        console.log('Error:', data);
+                        $('#saveBtn').html('Save Changes');
+                    }
+                });
+            });
+
+            $('body').on('click', '.deleteProduct', function () {
+                var product_id = $(this).data("id");
+                var result = confirm("Are you sure want to delete this product?");
+                if(result) {
+                    $.ajax({
+                        type: "DELETE",
+                        url: "{{ route('products.store') }}"+'/'+product_id,
+                        success: function (data) {
+                            table.draw();
+                        },
+                        error: function (data) {
+                            console.log('Error:', data);
+                        }
+                    });
+                }
+            });
+
+        });
+    </script>
+@endsection
